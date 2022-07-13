bd5700de81e3a3042e8c9ed6c6b7b0dbb4b72b4c
Adrian <adrian.talonia11@gmail.com>
2022-07-12 23:26:29 -0500

    using tsconfig to verify rules about code

--- typescript_concepts/fundamentals/helloworld/app.js
+++ typescript_concepts/fundamentals/helloworld/app.js
@@ -1,4 +1,9 @@
 "use strict";
-const msg = 'Hola mundo';
+const msg = "Hola mundo";
 //msg = 'Hola'
 //msg = 1
+const hero = {
+    name: "Ironman",
+    age: 45,
+};
+console.log(hero.age);
--- typescript_concepts/fundamentals/helloworld/app.ts
+++ typescript_concepts/fundamentals/helloworld/app.ts
@@ -1,3 +1,10 @@
-const msg: string = 'Hola mundo'
+const msg: string = "Hola mundo";
 //msg = 'Hola'
 //msg = 1
+
+const hero = {
+  name: "Ironman",
+  age: 45,
+};
+
+console.log(hero.age);

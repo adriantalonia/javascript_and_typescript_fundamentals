9cad658adabb39e6caede6ff08540a40542eb100
Adrian <adrian.talonia11@gmail.com>
2022-10-04 23:07:40 -0500

    objects

---
+++ typescript_concepts/02Functions/objects/objects.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let flash = {
+        name: "Barry Allen",
+        age: 24,
+        power: ["speed", "time travel"]
+    };
+    flash = {
+        name: "another name",
+        age: 60,
+        power: ["strong"]
+    };
+})();
---
+++ typescript_concepts/02Functions/objects/objects.ts
@@ -0,0 +1,13 @@
+(() => {
+  let flash = {
+    name: "Barry Allen",
+    age: 24,
+    power: ["speed", "time travel"]
+  };
+
+  flash = {
+    name: "another name",
+    age: 60,
+    power: ["strong"]
+  };
+})();
